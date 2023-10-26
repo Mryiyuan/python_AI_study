15af70cdd26b6b1d114774e3b5cfecad2288cc79
Mryiyuan <574306720@qq.com>
2023-10-26 11:35:32 +0800

    修改程序

--- Rnn_Mnist/test_one_img.py
+++ Rnn_Mnist/test_one_img.py
@@ -4,24 +4,26 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import pandas as pd
 
-new_model = tf.keras.models.load_model('model.h5')
+def test():
+    new_model = tf.keras.models.load_model('model.h5')
 
-# 调用模型进行预测识别
-im = Image.open(r"7.png")  # 读取图片路径
-im = im.resize((28, 28))  # 调整大小和模型输入大小一致
-im = np.array(im)
+    # 调用模型进行预测识别
+    im = Image.open(r"7.png")  # 读取图片路径
+    im = im.resize((28, 28))  # 调整大小和模型输入大小一致
+    im = np.array(im)
 
-# 对图片进行灰度化处理
-p3 = im.min(axis=-1)
-# plt.imshow(p3, cmap='gray')
+    # 对图片进行灰度化处理
+    p3 = im.min(axis=-1)
+    # plt.imshow(p3, cmap='gray')
 
-# 将白底黑字变成黑底白字   由于训练模型是这种格式
-for i in range(28):
-    for j in range(28):
-        p3[i][j] = 255-p3[i][j]
+    # 将白底黑字变成黑底白字   由于训练模型是这种格式
+    for i in range(28):
+        for j in range(28):
+            p3[i][j] = 255 - p3[i][j]
 
-# 模型输出结果是每个类别的概率，取最大的概率的类别就是预测的结果
-ret = new_model.predict((p3 / 255).reshape((1, 28, 28)))
-print(ret)
-number = np.argmax(ret)
-print(number)
+    # 模型输出结果是每个类别的概率，取最大的概率的类别就是预测的结果
+    ret = new_model.predict((p3 / 255).reshape((1, 28, 28)))
+    print(ret)
+    number = np.argmax(ret)
+    print(number)
+
